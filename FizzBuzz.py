b33082e07c8e64d3dfd00d7b5314c55fcd000e66
jakablazon <jakablazon@Jakas-MacBook-Pro.local>
2016-07-29 18:53:54 +0200

    init comitt

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+i = int(raw_input("Vnesite število med 1 in 100: "))
+if i < 1 or i > 100:
+    print "Vnesli ste napačno število!"
+else:
+    for x in range(1,i+1):
+        if x % 3 != 0 and x % 5 != 0:
+            print x
+        if x % 3 == 0 and x % 5 == 0:
+            print "fizzbuzz"
+        elif x % 3 == 0:
+            print "fizz"
+        elif x % 5 == 0:
+            print "buzz"
+
